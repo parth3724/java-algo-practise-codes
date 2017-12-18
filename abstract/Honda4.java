058b962c6817aad4593c7cc58fd8782b74a8946f
Dudhatra <pddudhat@8c85902ebedc.ant.amazon.com>
2017-12-18 19:46:22 +0530

    Dummy commit to learn git

--- abstract/Honda4.java
+++ abstract/Honda4.java
@@ -23,7 +23,7 @@ int pq = 1;
 class Honda4 extends Bike implements int2{  
 void run(){  class local{static final int a = 0;}
 System.out.println("honda running safely.."+int2.pq);}  
-  
+  //// random comment for learning git
 public static void main(String args[]){  
  Bike obj = new Honda4();  
  obj.run();  
